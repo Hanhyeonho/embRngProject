71454e336ea5e422b50811ff7896d1747b477818
seok-ho <osh8777@naver.com>
2013-11-30 03:16:48 +0900

    fin v0.1  - add Device manage authority to v0.2

--- emb_ver01/src/com/example/emb_ver01/MainActivity.java
+++ emb_ver01/src/com/example/emb_ver01/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.emb_ver01;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		getAdmin();
 	}
 
 	@Override
@@ -17,6 +22,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void getAdmin() {
+		ComponentName adminComponent = new ComponentName(this, DPMReciver.class);
+		
+		Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+		intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminComponent);
+		intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"Needs to be added");
+		startActivityForResult(intent, 1);
 	}
 
 }
